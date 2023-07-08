ff659f835192ae8220c5a3705135577d4d9c9d39
Icaro Albar <icaro.albar@live.com>
2023-07-08 20:52:41 -0300

    :sparkles: feat: New function
    Telegraf configured

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,33 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Telegraf } from "telegraf";
 
-type Data = {
-  name: string
-}
-
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const bot = new Telegraf(`${process.env.NEXT_PUBLIC_BOT_TOKEN}`);
+  const { name, email, phone, message } = req.body;
+
+  bot.telegram
+    .sendMessage(
+      `${process.env.NEXT_PUBLIC_BOT_USER}`,
+      `  
+            Menssagem do site AGÁ Empreendimentos:
+            
+            Nome:  ${name}
+        
+            E-mail:  ${email}  
+        
+            Telefone:  ${phone}
+            
+            Mensagem:
+            ${message}
+        `
+    )
+    .then((message) => {
+      console.log(message);
+      res.status(200).json({ message: "Mensagem enviada com sucesso!" });
+    })
+    .catch((err) => console.log(err));
 }
--- src/partials/contact/index.tsx
+++ src/partials/contact/index.tsx
@@ -1,11 +1,14 @@
-import { ReactElement } from "react";
-import { data, dataInput } from "./data";
+import { ReactElement, useState } from "react";
+import { data } from "./data";
 import { Input } from "../../components/Input";
 import { Textarea } from "../../components/Textarea";
 import { Button } from "../../components/Button";
 import { useForm, SubmitHandler } from "react-hook-form";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
 
 export function Contact(): ReactElement {
+  const [buttonDisables, setButtonDisabled] = useState<boolean>(false);
   const {
     register,
     handleSubmit,
@@ -21,47 +24,60 @@ export function Contact(): ReactElement {
   });
 
   const handleSend: SubmitHandler<any> = (data: any) => {
-    console.log(data);
+    setButtonDisabled(true);
+    axios.post("/api/hello", data);
     reset();
+    toast.success("Cliente cadastrado com sucesso!", {
+      position: toast.POSITION.TOP_RIGHT,
+    });
+    setButtonDisabled(false);
   };
 
   return (
-    <section className="grid grid-cols-2 border-b-2 border-secondary-10">
-      <div className="col-span-1 bg-contact bg-cover h-[90vh]" />
-      <div className="col-span-1 bg-primary-10 p-10">
-        {data.map((item, index) => (
-          <div
-            key={index}
-            className="text-white text-center flex flex-col gap-2"
+    <section>
+      <ToastContainer />
+      <div className="grid grid-cols-2 border-b-2 border-secondary-10">
+        <div className="col-span-1 bg-contact bg-cover h-[90vh]" />
+        <div className="col-span-1 bg-primary-10 p-10">
+          {data.map((item, index) => (
+            <div
+              key={index}
+              className="text-white text-center flex flex-col gap-2"
+            >
+              <h1 className="text-2xl">{item.title}</h1>
+              <p className="font-light">{item.subtitle}</p>
+            </div>
+          ))}
+          <form
+            onSubmit={handleSubmit(handleSend)}
+            className="flex flex-col gap-3 mt-10"
           >
-            <h1 className="text-2xl">{item.title}</h1>
-            <p className="font-light">{item.subtitle}</p>
-          </div>
-        ))}
-        <form
-          onSubmit={handleSubmit(handleSend)}
-          className="flex flex-col gap-3 mt-10"
-        >
-          <Input name="name" placeholder="Nome*" register={register} />
-          <Input
-            type="email"
-            name="email"
-            placeholder="E-mail*"
-            register={register}
-          />
-          <Input
-            type="number"
-            name="phone"
-            placeholder="Telefone*"
-            register={register}
-          />
-          <Textarea
-            name="message"
-            placeholder="Digite sua mensagem*"
-            register={register}
-          />
-          <Button type="submit" title="enviar" size="full" />
-        </form>
+            <Input name="name" placeholder="Nome*" register={register} />
+            <Input
+              type="email"
+              name="email"
+              placeholder="E-mail*"
+              register={register}
+            />
+            <Input
+              type="number"
+              name="phone"
+              placeholder="Telefone*"
+              register={register}
+            />
+            <Textarea
+              name="message"
+              placeholder="Digite sua mensagem*"
+              register={register}
+            />
+            <Button
+              type="submit"
+              title="enviar"
+              size="full"
+              disabled={buttonDisables}
+            />
+          </form>
+        </div>
       </div>
     </section>
   );
